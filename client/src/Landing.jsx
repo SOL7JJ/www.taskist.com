b0108ad69f6da2ec40daa65c179ad9770a38f63e
jamesjonathantossou-ayayi <jonathanelie007@gmail.com>
2026-01-27 15:24:03 +0000

    Fix Landing.css import casing for Linux builds

--- client/src/Landing.jsx
+++ client/src/Landing.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./landing.css";
+import "./Landing.css";
 
 export default function Landing() {
   const [menuOpen, setMenuOpen] = useState(false);
